bdb47edb50e95d9244c5ee15f5ab0fa26136acdb
Frenk1 <f1.frenk@gmail.com>
2013-02-01 00:30:19 +0800

    sape test controller

---
+++ application/controllers/sape.php
@@ -0,0 +1,51 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Sape extends CI_Controller {
+
+    public $_sapeapi;
+    public $_get_user;
+    public $_model_messages;
+    private $_manager;
+
+    public function __construct() {
+        parent::__construct();
+
+        # инициализация библиотеки для работы с запросами xml-rpc sape.ru
+        $auth_data = array(
+                'login' => 'login here',
+                'password' => 'md5 hash here' // md5 hash
+            );
+        $this->load->library('sapeapi', $auth_data);
+        $this->_sapeapi = $this->sapeapi->set_debug(0);
+
+        $user = $this
+                ->_sapeapi
+                ->query('sape.get_user')
+                ->xml_cache()
+                ->fetch()
+                ->get_xml();
+
+        var_dump( $user );
+
+        #получение первичных данных. Нужно для авторизации и получения баланса юзера
+        $this->_get_user = $this->get_user();
+    }
+
+    /**
+    * ремаппинг методов роутинга, чтобы легче было отличать функции от страниц
+    */
+    public function _remap($method, $params = array()) {
+        $method = 'action_' . $method;
+        if (method_exists($this, $method)) {
+            return call_user_func_array(array($this, $method), $params);
+        }
+        show_404();
+    }
+
+    function action_index() {
+        // костыль. Если есть параметры, когда они не нужны - то показать 404
+        if (func_num_args()) {
+            show_404();
+        }
+    }
+}

5dc6f09284fde9a418972d83b0f34f50ed28aacb
Frenk1 <f1.frenk@gmail.com>
2013-02-06 01:58:30 +0800

    фикс генерирования вызова модели
    передача данных и от sape

--- application/libraries/SapeApiLoader.php
+++ application/libraries/SapeApiLoader.php
@@ -9,6 +9,14 @@ class SapeApiLoader {
             'optimizator' => array()
         );
 
+    /**
+    * название модели
+    */
+    private $_model_name = false;
+
+    /**
+    * используемая модель
+    */
     private $_model = false;
 
     /**
@@ -25,7 +33,7 @@ class SapeApiLoader {
         $this->CI =& get_instance();
 
         $this->_loader['webmaster'] = array(
-                'sape.get_user'          => 'Get_user',
+                'sape.get_user'          => 'Get_user', // invoke example - $this->CI->Get_user->get_test()
                 'sape.get_balance'       => 'Get_balance',
                 'sape.get_balance_real'  => 'Get_balance_real',
                 'sape.get_balance_locks' => 'Get_balance_locks'
@@ -37,7 +45,20 @@ class SapeApiLoader {
         if ($this->detect_model($query)) {
             log_message('debug', 'SapeApiLoader try load model for query: ' . $query);
             $this->CI->load->model($this->_model_path);
+            $this->_model = class_exists($this->_model_name) ? $this->CI->{$this->_model_name} : false;
+            try {
+                if (!$this->_model) {
+                    throw new SapeApiLoaderException('Can not load model for query: ' . $query);
+                }
+            } catch (SapeApiLoaderException $e) {
+                echo $e->getMessage();
+            }
         }
+    }
+
+    protected function set_data($data) {
+        log_message('debug', 'SapeApiLoader method _set_data cannot sync with databse');
+        $this->_model && method_exists($this->_model, '_set_data') ? $this->_model->_set_data($data) : '';
     }
 
     /** определение модели по запросу */
@@ -45,8 +66,8 @@ class SapeApiLoader {
         try {
             foreach ($this->_loader as $method_type => $methods) {
                 if (array_key_exists($query, $methods)) {
-                    $this->_model = $this->_loader[$method_type][$query];
-                    $this->_model_path = 'sape/' . $method_type . '/' . $this->_model;
+                    $this->_model_name = $this->_loader[$method_type][$query];
+                    $this->_model_path = 'sape/' . $method_type . '/' . $this->_model_name;
                     log_message('debug', 'SapeApiLoader detect model for query: ' . $query);
                     break;
                 }

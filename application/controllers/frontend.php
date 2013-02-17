48861d00e324e8e9629493019c61c39c65192404
Frenk1 <f1.frenk@gmail.com>
2013-02-17 13:23:35 +0800

    Будущий контроллер интерфейса

---
+++ application/controllers/frontend.php
@@ -0,0 +1,12 @@
+<?
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Frontend extends CI_Controller {
+
+    public function index() {
+        var_dump(1);
+    }
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
